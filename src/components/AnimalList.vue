6a22d44bc767eabfd4afde8c56a8f91b535678a4
uros.purtic <uros.purtic@gmail.com>
2018-04-26 16:17:12 +0200

    settimeout button

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,146 +1,171 @@
 <template>
-  <div>
+    <div>
 
-    <h4>add another animal to our zoo</h4>
+        <h4>add another animal to our zoo</h4>
 
-    <form @submit.prevent="addAnimal">
+        <form @submit.prevent="addAnimal">
 
-      <label>Add animal type</label>
-      <input v-model="newAnimal.type"  type="text" placeholder="animal type"/>
+            <label>Add animal type</label>
+            <input v-model="newAnimal.type" type="text" placeholder="animal type"/>
+            <p> {{ newAnimal.type }}</p>
 
-
-      <label>Add animal name</label>
-      <input v-model="newAnimal.name" type="text" placeholder="animal name"/>
-
-
-      <label>Add animal date of birth</label>
-      <input type="text" v-model="newAnimal.dateOfBirth" placeholder="date of birth"/>
+            <label>Add animal name</label>
+            <input v-model="newAnimal.name" type="text" placeholder="animal name"/>
 
 
-              <button type="submit">add animal</button>
-
-    </form>
-
-
-    <h3>List of animals in the local zoo</h3>
-
-    <table>
-
-      <thead>
-      <th>Animal Type</th>
-      <th>Name</th>
-      <th>date of birth</th>
-      <th>remove from the list</th>
-      <th>option</th>
-
-      </thead>
-
-      <tbody>
-      <tr v-for="(animal,key) in animals" :key="key">
-
-        <td>{{ animal.type }}</td>
-        <td>{{ animal.name }}</td>
-
-        <td v-if="animal.dateOfBirth != ''">{{ animal.dateOfBirth }}</td>
-        <td v-else> Nepoznat </td>
-        <td><button @click="removeAnimal(animal)">remove </button></td>
-        <td><button @click="moveToTop(key)">move to the top</button></td>
-
-      </tr>
-
-      </tbody>
+            <label>Add animal date of birth</label>
+            <input type="text" v-model="newAnimal.dateOfBirth" placeholder="date of birth"/>
 
 
+            <button type="submit">add animal</button>
 
-    </table>
+        </form>
 
-  </div>
+
+        <h3>List of animals in the local zoo</h3>
+
+        <table>
+
+            <thead>
+            <th>Animal Type</th>
+            <th>Name</th>
+            <th>date of birth</th>
+            <th>remove from the list</th>
+            <th>option</th>
+
+            </thead>
+
+            <tbody>
+            <tr v-for="(animal,key) in animals" :key="key">
+
+                <td>{{ animal.type }}</td>
+                <td>{{ animal.name }}</td>
+
+                <td v-if="animal.dateOfBirth != ''">{{ animal.dateOfBirth }}</td>
+                <td v-else> Nepoznat</td>
+                <td>
+                    <button @click="removeAnimal(animal)">remove</button>
+                </td>
+                <td>
+                    <button @click="moveToTop(key)">move to the top</button>
+                </td>
+
+            </tr>
+
+            </tbody>
+
+
+        </table>
+
+        <div style="padding-top: 50px">
+
+            <button @click="disabelButton" :disabled="isButtonDisabled"> random button</button>
+
+        </div>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'AnimalList',
- data(){
+    export default {
+        name: 'AnimalList',
+        data() {
 
-      return{
-          animals:[
-
-              {
-                  type: 'reptile',
-                  name: 'Sima the Lizard',
-                  dateOfBirth: '25.02.1988'
-              },
-
-              {
-                  type: 'insect',
-                  name: 'Pera the Fly',
-                  dateOfBirth: '25.05.1955'
-              },
-
-              {
-                  type: 'sponge',
-                  name: 'SpongeBob',
-                  dateOfBirth: '25.02.1980'
-              },
-
-              {
-                  type: 'amphibious ',
-                  name: 'John the Frog',
-                  dateOfBirth: '25.02.1988'
-              },
-
-              {
-                  type: 'mammals',
-                  name: 'Timmy the Bear',
-                  dateOfBirth: '25.08.2005'
-              },
-
-              {
-                  type: 'mammals',
-                  name: 'Timmy the Lion',
-                  dateOfBirth: '',
-              },
+            return {
 
 
-          ],
+                isButtonDisabled:false,
 
-          newAnimal:{
+                animals: [
 
-              type: '',
-              name: '',
-              dateOfBirth: '',
-          }
+                    {
+                        type: 'reptile',
+                        name: 'Sima the Lizard',
+                        dateOfBirth: '25.02.1988'
+                    },
+
+                    {
+                        type: 'insect',
+                        name: 'Pera the Fly',
+                        dateOfBirth: '25.05.1955'
+                    },
+
+                    {
+                        type: 'sponge',
+                        name: 'SpongeBob',
+                        dateOfBirth: '25.02.1980'
+                    },
+
+                    {
+                        type: 'amphibious ',
+                        name: 'John the Frog',
+                        dateOfBirth: '25.02.1988'
+                    },
+
+                    {
+                        type: 'mammals',
+                        name: 'Timmy the Bear',
+                        dateOfBirth: '25.08.2005'
+                    },
+
+                    {
+                        type: 'mammals',
+                        name: 'Timmy the Lion',
+                        dateOfBirth: '',
+                    },
 
 
+                ],
+
+                newAnimal: {
+
+                    type: '',
+                    name: '',
+                    dateOfBirth: '',
+                }
 
 
-      }
+            }
 
 
- },
+        },
 
- methods: {
+        methods: {
 
-     removeAnimal(animal){
+            removeAnimal(animal) {
 
-         this.animals.splice(this.animals.indexOf(animal),1);
-
-
-     },
-
-     moveToTop(key){
-
-         this.animals.unshift(this.animals.splice(key,1)[0])
-
-     },
-
-     addAnimal(){
-
-         this.animals.push(this.newAnimal)
-
-     }
+                this.animals.splice(this.animals.indexOf(animal), 1);
 
 
- },
-}
+            },
+
+            moveToTop(key) {
+
+                this.animals.unshift(this.animals.splice(key, 1)[0])
+
+            },
+
+            addAnimal() {
+
+                this.animals.push(this.newAnimal)
+
+            },
+
+            //https://stackoverflow.com/questions/37465289/how-to-set-timeout-in-a-vuejs-method
+
+            disabelButton() {
+
+                this.isButtonDisabled = true;
+
+                setTimeout(function (){
+                    this.isButtonDisabled = false
+
+                }, 5000)
+            }
+
+
+        }
+
+
+    }
 </script>
